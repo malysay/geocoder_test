5bca8501f8670894905089f5ff79ba387aae7342
Vladislav Aprosimov <avl@labis.su>
2023-09-18 02:30:11 +0900

    add leaflet-geosearch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
-import React from "react";
-import Map from "./Map";
-import { MapContainer } from "react-leaflet";
+import React, { Component } from 'react';
+import './App.css';
+import MapComponent from './Map';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Leaflet Map Example</h1>
-      <MapContainer center={[62.0338900, 129.7330600]} zoom={13}>
-        <Map />
-      </MapContainer>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '', // Здесь будет храниться запрос пользователя
+      searchResult: null, // Здесь будут храниться результаты геокодирования
+    };
+  }
+
+  handleQueryChange = (e) => {
+    this.setState({ query: e.target.value });
+  };
+
+  handleSearch = () => {
+    const { query } = this.state;
+    const provider = new OpenStreetMapProvider();
+
+    // Выполняем геокодирование с использованием OpenStreetMapProvider
+    provider
+      .search({ query })
+      .then((result) => {
+        this.setState({ searchResult: result });
+      })
+      .catch((error) => {
+        console.error('Ошибка геокодирования:', error);
+      });
+  };
+
+  render() {
+    const { searchResult } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Leaflet Map Example</h1>
+        <div>
+          <input
+            type="text"
+            placeholder="Введите адрес для поиска"
+            value={this.state.query}
+            onChange={this.handleQueryChange}
+          />
+          <button onClick={this.handleSearch}>Поиск</button>
+        </div>
+        <main>
+          <MapComponent searchResult={searchResult} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Map.js
+++ src/Map.js
@@ -1,140 +1,81 @@
-import React, { useRef, useEffect, useState } from 'react';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-import { useMap, MapContainer, TileLayer, Marker } from 'react-leaflet';
-import { SearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
-import { Typeahead } from 'react-bootstrap-typeahead';
+import React, { Component } from "react";
+import { MapContainer, TileLayer, CircleMarker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
 
-import 'react-bootstrap-typeahead/css/Typeahead.css';
+class MapComponent extends Component {
+  componentDidMount() {
+    const provider = new OpenStreetMapProvider();
+    const searchControl = new GeoSearchControl({
+      provider,
+      style: "bar",
+      showMarker: true,
+    });
 
-function Map() {
-  const mapRef = useRef(null);
-  const [searchResults, setSearchResults] = useState([]);
-  const [selectedAddress, setSelectedAddress] = useState('');
-  const map = useMap();
-
-  useEffect(() => {
-    if (!mapRef.current) {
-      mapRef.current = map;
-
-      // Добавление слоя карты
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(map);
-      
-      // Создание поискового провайдера
-      const provider = new OpenStreetMapProvider();
-
-      // Создание поискового контрола
-      const searchControl = new SearchControl({
-        provider,
-        position: 'topright',
-        showMarker: true,
-        showPopup: false,
-        marker: {
-          icon: new L.Icon.Default(),
-          draggable: false,
-        },
-        maxMarkers: 1,
-        autoCollapse: true,
-        retainZoomLevel: false,
-        animateZoom: true,
-        searchLabel: 'Поиск адреса...',
-        notFoundMessage: 'Адрес не найден',
-        messageHideDelay: 3000,
-        animate: true,
-      });
-
-      // Добавление поискового контрола к карте
-      map.addControl(searchControl);
-      
-      // Обработчик выбора адреса
-      map.on('geosearch/showlocation', (e) => {
-        const { lat, lng } = e.Location;
-        setSelectedAddress(e.Location.label);
-        map.setView([lat, lng], 13);
+    if (this.mapRef) {
+      // Дождитесь, пока MapContainer будет создан
+      this.mapRef.whenCreated((map) => {
+        map.addControl(searchControl);
       });
     }
-  }, [map]);
+  }
 
-  return (
-    <div>
-      <div id="map" style={{ width: '100%', height: '80vh' }}></div>
-      <div>
-        {selectedAddress && <p>Выбранный адрес: {selectedAddress}</p>}
-      </div>
-    </div>
-  );
+  componentDidUpdate(prevProps) {
+    const { searchResult } = this.props;
+
+    // Проверяем, изменились ли результаты поиска
+    if (
+      searchResult !== prevProps.searchResult &&
+      searchResult &&
+      searchResult.length > 0
+    ) {
+      // Получаем координаты первого результата (первого местоположения)
+      const firstResult = searchResult[0];
+      const newPosition = [firstResult.y, firstResult.x];
+
+      // Устанавливаем новый зум
+      const newZoom = 15;
+
+      // Плавно перемещаем карту к новому местоположению с анимацией
+      this.mapRef.flyTo(newPosition, newZoom, {
+        duration: 1, // Длительность анимации в секундах
+      });
+    }
+  }
+
+  render() {
+    const { searchResult } = this.props;
+    const position = [62.03389, 129.73306]; // Начальные координаты карты
+    const mapZoom = 13;
+
+    return (
+      <MapContainer
+        ref={(ref) => {
+          this.mapRef = ref;
+        }}
+        center={position}
+        zoom={mapZoom}
+        style={{ width: "80%", height: "800px" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {searchResult &&
+          searchResult.map((result, index) => (
+            <CircleMarker
+              center={[result.y, result.x]}
+              radius={10} // Радиус круга
+              color="blue" // Цвет обводки
+              fillColor="cyan" // Цвет заливки
+              fillOpacity={0.6} // Прозрачность заливки
+            >
+              <Popup>{result.label}</Popup>
+            </CircleMarker>
+          ))}
+      </MapContainer>
+    );
+  }
 }
 
-export default Map;
-
-
-// import React, { useRef, useEffect, useState } from 'react';
-// import 'leaflet/dist/leaflet.css';
-// import L from 'leaflet';
-// import { useMap } from 'react-leaflet';
-// import { SearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
-// import { Typeahead } from 'react-bootstrap-typeahead';
-
-// import 'react-bootstrap-typeahead/css/Typeahead.css'; // Импортируйте стили для Typeahead
-
-// function Map() {
-//   const mapRef = useRef(null);
-//   const [searchResults, setSearchResults] = useState([]);
-//   const [selectedAddress, setSelectedAddress] = useState('');
-//   const map = useMap();
-
-//   useEffect(() => {
-//     if (!mapRef.current) {
-//       mapRef.current = map;
-
-//       // Добавление слоя карты
-//       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//       }).addTo(map);
-//     }
-//   }, [map]);
-
-//   const handleSearch = (query) => {
-//     const provider = new OpenStreetMapProvider();
-//     provider.search({ query })
-//       .then((results) => {
-//         setSearchResults(results);
-//       })
-//       .catch((error) => {
-//         console.error(error);
-//         setSearchResults([]);
-//       });
-//   };
-
-//   const handleResultSelect = (selected) => {
-//     if (selected.length > 0) {
-//       const { x, y } = selected[0];
-//       map.setView([y, x], 13);
-//       setSelectedAddress(selected[0].label);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-//         <Typeahead
-//           id="address-search"
-//           labelKey="label"
-//           options={searchResults}
-//           placeholder="Введите адрес"
-//           onChange={handleResultSelect}
-//           onInputChange={handleSearch}
-//         />
-//         <button onClick={() => setSelectedAddress('')}>Сбросить</button>
-//       </div>
-//       <div id="map" style={{ width: '100%', height: '80vh' }}></div>
-//       <div>
-//         {selectedAddress && <p>Выбранный адрес: {selectedAddress}</p>}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Map;
+export default MapComponent;
